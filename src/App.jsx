b43b3dbdfef03ba64deb438820549fc4592e2cc2
nabaa-dev <nabaaadnan284@gmil.com>
2026-07-29 00:36:47 +0300

    style: fix typo in name and improve component indentation formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,23 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-function App(){
+function App() {
   const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode ? "dark" : "light"}>
-    <Navbar/>
-    <button onClick={() => setDarkMode(!darkMode)}>
-   {darkMode ? "☀️ نهاري" : "🌙 ليلي"}
-   </button>
+      <Navbar />
+      <button onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? "☀️ نهاري" : "🌙 ليلي"}
+      </button>
 
-    <Hero name="مرحبا انا نباً عدنان" jop="Frontend Devloper" desc="React Devloper"/>
-    <About/>
-    <Skills/>
-    <Projects/>
-    <Contact/>
-    <Footer/>
+
+
+      <Hero name="مرحبا انا نبأ عدنان" jop="Frontend Devloper" desc="React Devloper" />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
